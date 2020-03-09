d42d9c9595b9f05e9e617409a68efc6338d4b01e
anhpq.adsplus <anhpq.adsplus@gmail.com>
2020-03-09 10:43:09 +0700

    create branch develop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <v-btn @click="logOut()">Click here</v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    logOut(){
+      this.$router.push('/login')
+    }
   }
 }
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col xs="12" sm="12" md="4" lg="4" xl="4">
+                <v-btn @click="testMethod()">Click to test</v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+    // import axios from 'axios'
+    export default {
+        data(){
+            return {
+
+            }
+        },
+        methods: {
+            testMethod(){
+                alert('dm')
+                this.$router.push('/')
+            }
+        },
+        created(){
+            this.testMethod();
+        }
+    }
+</script>
+<style scoped>
+
+</style>
